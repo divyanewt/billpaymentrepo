39a74d4fec47e8192c50ec80d4cc5a9178b8a8f2
Hvuser <Hvuser@EBHVR02-03.corp.hexaware.com>
2018-04-30 10:33:03 +0530

    second commit

--- src/main/java/com/hexaware/billsystem/model/Employee.java
+++ src/main/java/com/hexaware/billsystem/model/Employee.java
@@ -1,13 +1,8 @@
 package com.hexaware.billsystem.model;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.Id;
 
-import org.springframework.stereotype.Component;
 
-/*@Component*/
-@Entity
+
 public class Employee 
 {
 	
